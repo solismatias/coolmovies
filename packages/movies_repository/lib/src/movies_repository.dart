5e50595ef6d86dd9cd8e0afc6577f0482feb8e54
Matias Solis <67497671+solismatias@users.noreply.github.com>
2024-05-27 17:40:38 -0300

    feat(packages): create function to get reviews of a movie

--- packages/movies_repository/lib/src/movies_repository.dart
+++ packages/movies_repository/lib/src/movies_repository.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:movies_repository/src/graphql_config.dart';
 import 'package:movies_repository/src/models/movie_model.dart';
+import 'package:movies_repository/src/models/review_model.dart';
 
 class MoviesRepository {
   static GraphQLConfig graphQLConfig = GraphQLConfig();
@@ -80,6 +81,48 @@ class MoviesRepository {
 
       var movieData = result.data!['movieById'];
       return MovieModel.fromMap(map: movieData);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<List<ReviewModel>> getMovieReviews(String movieId) async {
+    try {
+      QueryResult result = await client.query(
+        QueryOptions(
+          document: gql("""
+          query MyQuery(\$id: UUID!) {
+            allMovieReviews(condition: {movieId: \$id}) {
+              edges {
+                node {
+                  id
+                  body
+                  movieId
+                  rating
+                  title
+                  userReviewerId
+                }
+              }
+            }
+          }
+          """),
+          variables: {
+            'id': movieId,
+          },
+        ),
+      );
+
+      if (result.hasException) throw Exception(result.exception);
+
+      List reviewData = result.data!['allMovieReviews']['edges'] ?? [];
+
+      List<ReviewModel> reviews = [];
+
+      for (var review in reviewData) {
+        reviews.add(ReviewModel.fromMap(map: review['node']));
+      }
+
+      return reviews;
     } catch (e) {
       throw Exception(e);
     }
