02a6ebbcb0e52ff42f2f5dcb351cf94e25282c0c
Matias Solis <67497671+solismatias@users.noreply.github.com>
2024-05-27 15:11:30 -0300

    feat(packages): update MovieModel with optional director properties and release date

--- packages/movies_repository/lib/src/movie_model.dart
+++ packages/movies_repository/lib/src/movie_model.dart
@@ -2,17 +2,29 @@ class MovieModel {
   final String id;
   final String imgUrl;
   final String title;
+  final String? releaseDate;
+  final String? directorId;
+  final String? directorAge;
+  final String? directorName;
 
   const MovieModel({
     required this.id,
     required this.imgUrl,
     required this.title,
+    this.releaseDate = '',
+    this.directorId = '',
+    this.directorAge = '',
+    this.directorName = '',
   });
 
   static MovieModel fromMap({required Map map}) => MovieModel(
         id: map['id'],
         imgUrl: map['imgUrl'],
         title: map['title'],
+        releaseDate: map['releaseDate'],
+        directorId: map['directorId'],
+        directorAge: map['directorAge'],
+        directorName: map['directorName'],
       );
 
   static const empty = MovieModel(id: '', imgUrl: '', title: '');
