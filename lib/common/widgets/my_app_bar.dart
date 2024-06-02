55144b758fb59be7105456a90a4c17e5ade8edc4
Matias Solis <67497671+solismatias@users.noreply.github.com>
2024-06-02 20:02:33 -0300

    feat: add about dialog

--- lib/common/widgets/my_app_bar.dart
+++ lib/common/widgets/my_app_bar.dart
@@ -1,13 +1,18 @@
+import 'package:coolmovies/common/widgets/show_feed_back.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // For Clipboard
+
 import 'package:coolmovies/common/constants/app_layout.dart';
 import 'package:coolmovies/common/utils/utils.dart';
-import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({
     super.key,
     this.showBackButton = true,
   });
+
   final bool showBackButton;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -24,6 +29,60 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
                 child: Image.asset('assets/logo.png'),
               ),
             ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.info_outline),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Row(
+                    children: [
+                      Image.asset('assets/logo.png', height: AppLayout.logoSmall),
+                      const SizedBox(width: AppLayout.spacingSmall),
+                      const Text('CoolMovies'),
+                    ],
+                  ),
+                  content: SingleChildScrollView(
+                    child: ListBody(
+                      children: [
+                        const Text('Developer: Matias Solis'),
+                        GestureDetector(
+                          onTap: () {
+                            Clipboard.setData(const ClipboardData(text: 'https://www.linkedin.com/in/solismatias/'));
+                            showFeedback(true, successMessage: 'LinkedIn URL copied to clipboard');
+                          },
+                          child: const Text(
+                            'linkedin.com/in/solismatias/',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                        const SizedBox(height: AppLayout.spacingSmall),
+                        const Text('✅ List all the available movies, showing at least the title.'),
+                        const Text('✅ Tapping on a movie must open a view page presenting all the available information to the user.'),
+                        const Text('✅ Each movie page must display all its reviews.'),
+                        const Text('✅ Each review should consist of (at least): title, body and stars (1-5).'),
+                        const Text('✅ The user should be able to create new reviews.'),
+                        const Text('✅ User should be able to see the reviews and the movies even if offline.'),
+                        const Text('✅ User should be able to create reviews offline and sync them when online.'),
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Close'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ],
     );
   }
 
