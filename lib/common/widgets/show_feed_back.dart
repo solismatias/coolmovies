24ec421171691afd54eb1775b93bd347994735ba
Matias Solis <67497671+solismatias@users.noreply.github.com>
2024-06-02 14:03:42 -0300

    style(widgets): tone down colors

--- lib/common/widgets/show_feed_back.dart
+++ lib/common/widgets/show_feed_back.dart
@@ -8,7 +8,7 @@ void showFeedback(bool success, {String? successMessage, String? errorMessage}) 
       success ? successMessage ?? 'Petition sent successfully!' : errorMessage ?? 'Failed to send petition.',
       style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
     ),
-    backgroundColor: success ? Colors.green : Colors.red,
+    backgroundColor: success ? Colors.green[800] : Colors.red[800],
   );
 
   scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
