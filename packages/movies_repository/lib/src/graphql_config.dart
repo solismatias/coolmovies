5db24562985d801075b1cf996e8dfce12971ec13
Matias Solis <67497671+solismatias@users.noreply.github.com>
2024-06-02 20:26:33 -0300

    chore: remove androind test link

--- packages/movies_repository/lib/src/graphql_config.dart
+++ packages/movies_repository/lib/src/graphql_config.dart
@@ -2,9 +2,7 @@ import 'dart:io';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphQLConfig {
-  static HttpLink httpLink = HttpLink(
-    Platform.isAndroid ? 'http://10.0.2.2:5001/graphql' : 'http://localhost:5001/graphql',
-  );
+  static HttpLink httpLink = HttpLink('http://localhost:5001/graphql');
 
   GraphQLClient clientToQuery() => GraphQLClient(
         link: httpLink,
