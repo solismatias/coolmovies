6f203d45e6e019b42c0a1c1eaeb6d619a57dfe41
Matias Solis <67497671+solismatias@users.noreply.github.com>
2024-06-02 19:08:45 -0300

    feat(widgets): add optional color property

--- lib/common/widgets/show_feed_back.dart
+++ lib/common/widgets/show_feed_back.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
-void showFeedback(bool success, {String? successMessage, String? errorMessage}) {
+void showFeedback(bool success, {String? successMessage, String? errorMessage, Color? color}) {
   final snackBar = SnackBar(
     content: Text(
       success ? successMessage ?? 'Petition sent successfully!' : errorMessage ?? 'Failed to send petition.',
       style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
     ),
-    backgroundColor: success ? Colors.green[800] : Colors.red[800],
+    backgroundColor: color ?? (success ? Colors.green[800] : Colors.red[800]),
   );
 
   scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
